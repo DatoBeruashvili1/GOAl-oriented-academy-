fe31bcb7cd09430f0c1cdc3614f7d4c015214a09
DatoBeruashvili1 <datobero011@gmail.com>
2024-02-25 07:27:41 -0800

    commit

---
+++ day4/day4.py
@@ -0,0 +1,23 @@
+
+name = input(' please enter your name')
+
+surname = input('please enter your surname')
+
+age = input('please enter your age')
+
+
+
+print( name, surname,age)
+
+input('5+11 ?')
+
+ans = input ('2 . which academy is the best? ')
+if ans . lower() == 'goa' :
+
+ score = 1
+
+
+ print('CORRECT!')
+
+else :
+  print('INCORRECT!')
