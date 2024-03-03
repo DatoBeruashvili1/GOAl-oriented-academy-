8f39e3838b6a7c700116bd1b28f727573b4a396a
DatoBeruashvili1 <datobero011@gmail.com>
2024-03-03 07:32:54 -0800

    commit

---
+++ day5/day5.py
@@ -0,0 +1,72 @@
+num1 =int(input("enter the first number: "))
+num2 =int(input("enter the second number: "))
+
+
+
+print(type(num1))
+
+print(type(num2))
+
+print(int(num1 / num2))
+print(int(num1 // num2))
+print(int(num1 % num2))
+
+num3 = int(input("enter the third number: "))
+
+num4 = int(input("enter the fourth number :"))
+
+print(type(num3))
+
+print(type(num4))
+
+
+print(int(num3 / num4))
+print(int(num3 // num4))
+print(int(num3 % num4))
+
+
+print(type(num3))
+
+print(type(num4))
+
+num5 = int(input("enter the fivth number: "))
+
+num6 = int(input("enter the sixth number :"))
+
+print(int(num5 / num6))
+print(int(num5 // num6))
+print(int(num5 % num6))
+
+print(type(num5))
+
+print(type(num6))
+
+num7 = int(input("enter the seventh number: "))
+
+num8 = int(input("enter the eighth number :"))
+
+
+print(int(num7 / num8))
+print(int(num7 // num8))
+print(int(num7 % num8))
+
+print(type(num7))
+
+print(type(num8))
+
+
+
+
+num9 = int(input("enter the ninth number: "))
+
+num10 = int(input("enter the tenth number :"))
+
+print(int(num9 / num10))
+print(int(num9 // num10))
+print(int(num9 % num10))
+
+print(type(num9))
+
+print(type(num10))
+
+
