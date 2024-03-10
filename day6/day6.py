f9ad468c00d47558ab8f192a831095b04c774208
DatoBeruashvili1 <datobero011@gmail.com>
2024-03-10 23:22:11 +0400

    commit

---
+++ day6/day6.py
@@ -0,0 +1,107 @@
+num1 = input("please enter first number :")
+
+num2 = input("please enter second number :")  
+
+print(num1 == num2)
+
+print(19 > 43)
+
+print(101 > 98)
+
+print(1112 > 134)
+
+print(0 > -12)
+
+print(12 > 11)
+
+print(123 >= 121)
+
+print(6 >= 123)
+
+print(3 >= 3)
+
+print(432 >= 1121)
+
+print(10 >= -10)
+
+print(91 < 112)
+          
+print(12 < 90)
+
+
+print(7 < 77)
+
+print(7600 < -234)
+
+print(-666 < 3)
+
+
+print(10 <= 11)
+
+print(33 <= 33)
+
+print(121 <= 12)
+
+print(818 <= 818)
+
+print(120 <= 6)
+
+print(9==9)
+
+print(90 == 12)
+
+print(5121 == 901)
+
+print(200 == 90)
+
+print( 40 == 40)
+
+print(90 != 100)
+
+print(123 != 123)
+
+print(200 != 80)
+
+print(800 != 150)
+
+print(650 != 360)
+
+
+num3 = input("please enter third number :")
+
+num4 = input("please enter fourth number :")
+
+print(num3 > num4)
+
+print(num3 >= num4)
+
+print(num3 < num4)
+
+print(num3 <= num4)
+
+print(num3 == num4)
+
+print(num3 != num4)
+
+num5 = input("please enter fivth number :")
+
+
+print(num5 > num5 + "10")
+
+print(num5 >= num5 + "10")
+
+print(num5 < num5 + "10")
+
+print(num5 <= num5 + "10")
+
+print(num5 == num5 + "10")
+
+print(num5 != num5 + "10")
+
+num6 = input("please enter the integer :")
+
+num7 = input("please enter the float")
+
+print(num6 == num7)
+
+print(num6 != num7)
