8f53e0cc76466a8092ee846494a8cd7acf451379
DatoBeruashvili1 <datobero011@gmail.com>
2024-03-17 22:11:16 +0400

    commit

---
+++ day7/day7.py
@@ -0,0 +1,37 @@
+temperature = int(input("please enter temperature : "))  
+
+print((temperature * 1.8)+32)
+
+age = int(input("please enter your age"))
+
+print(age > 20 and age > 12)
+
+
+rectangle_angle1 =  int(input("please enter side of a triangle : "))
+
+
+rectangle_angle2 =  int(input("please enter second side of a triangle : "))
+
+
+rectangle_angle3 =  int(input("please enter third side of a triangle : "))
+
+num1 = int(input("please enter first number :"))
+
+print(num1 > 100 and num1 % 9 )
+
+
+print(True and False)
+
+print(True or False )
+
+print(True and True)
+
+print(True or True)
+
+print(False and False)
+
+print(False or False)
+
+
+
+
