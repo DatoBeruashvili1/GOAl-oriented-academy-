e9cb218f1d4325633c32820aa084135fe5984179
DatoBeruashvili1 <datobero011@gmail.com>
2024-04-01 17:39:17 +0400

    commit

--- day09/day09.py
+++ day09/day09.py
@@ -2,7 +2,7 @@ selery = int(input("please enter your selery :"))
 
 if selery >= 10000 : 
     print("goashi swavlobdi?")
-if selery == 1000 - 10000 :
+if selery >= 1000 :
     print("you mid")
 
 if selery < 1000 :
