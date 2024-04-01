0cc28643de7c30ceb2610768ab116ed5c7961b87
DatoBeruashvili1 <datobero011@gmail.com>
2024-04-01 16:41:55 +0400

    commit

---
+++ day09/classwork.py
@@ -0,0 +1,37 @@
+# num1 = 20
+
+# num2 = 19
+
+# if num1 > num2 :
+#     print("goa")
+
+
+mom = "natia"
+
+dad = "kaxa"
+
+sister = "mari"
+
+brother ="demetre"
+
+if mom == "natia" : 
+    print("mom")
+
+if dad == "kaxa" : 
+    print("dad")
+
+if sister == "mari" :
+    print("sister")
+
+if brother == "demetre" :
+    print("brother")
+
+
+
+
+
+
+
+
+
+
---
+++ day09/day09.py
@@ -0,0 +1,9 @@
+selery = int(input("please enter your selery :"))
+
+if selery >= 10000 : 
+    print("goashi swavlobdi?")
+if selery == 1000 - 10000 :
+    print("you mid")
+
+if selery < 1000 :
+    print("geswavla tavis droze goashi matricelo")
