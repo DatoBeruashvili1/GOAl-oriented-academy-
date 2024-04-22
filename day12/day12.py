b54d6ec99e662365ca9c3740763b5e78ee2e3ed2
DatoBeruashvili1 <datobero011@gmail.com>
2024-04-23 01:00:26 +0400

    commit

--- day12/day12.py
+++ day12/day12.py
@@ -1,27 +1,27 @@
-# name = input("please enter your name :")
+name = input("please enter your name :")
 
-# surname = input("please enter your surname :")
+surname = input("please enter your surname :")
 
-# age = int(input("please enter your age :"))
+age = int(input("please enter your age :"))
 
-# dwelling_place = input("please enter place where you live :")
+dwelling_place = input("please enter place where you live :")
 
-# about_user = []
+about_user = []
 
-# for i in name, surname, age, dwelling_place :
-#     about_user.append(i)
-# print(about_user)
-
-
-# print(about_user[0 : 2])
+for i in name, surname, age, dwelling_place :
+    about_user.append(i)
+print(about_user)
 
 
-# print(about_user[1 : 3])
+print(about_user[0 : 2])
 
 
-# print(about_user[0 : 3])
+print(about_user[1 : 3])
 
-# print(about_user[1 : 4 ])
+
+print(about_user[0 : 3])
+
+print(about_user[1 : 4 ])
 
 negative_number = int(input("please enter number under 0 :"))
 
@@ -37,31 +37,31 @@ elif negative_number < 0  :
 
 
 
-# me = ["dato", "beruashvili"]
+me = ["dato", "beruashvili"]
 
 
-# print(me[0 : 2])
+print(me[0 : 2])
 
 
-# favorite_movies = ["rush hour", "date time","koma", "carib seas pirates", "in vegas"]
+favorite_movies = ["rush hour", "date time","koma", "carib seas pirates", "in vegas"]
 
 
-# print(favorite_movies[0 : 2])
+print(favorite_movies[0 : 2])
 
-# print(favorite_movies[3 : 6])
+print(favorite_movies[3 : 6])
 
-# print(favorite_movies[1 :4])
+print(favorite_movies[1 :4])
 
-# print(favorite_movies[4 : 6])
+print(favorite_movies[4 : 6])
 
 
-# academy_name = input("please enter your academy name :")
+academy_name = input("please enter your academy name :")
 
-# if academy_name == "goa" :
-#     print("Goa is best choice")
+if academy_name == "goa" :
+    print("Goa is best choice")
 
-# else :
-#     print("wrong choice")
+else :
+    print("wrong choice")
 
 
 
