a31680a177576a6fd3229ad8164fd854d5a92ed9
DatoBeruashvili1 <datobero011@gmail.com>
2024-05-02 00:47:45 +0400

    commit

--- day24/day24.py
+++ day24/day24.py
@@ -1,59 +1,68 @@
-# my_list = [1,2,3,4,5,6,7,8,9,10]
+my_list = [1,2,3,4,5,6,7,8,9,10]
 
-# min = []
+min = []
 
-# mid = []
+mid = []
 
-# max = []
+max = []
 
-# for i in my_list :
-#     if i == 1 :
-#         min.append(i)
-#     if i == 10 :
-#         max.append(i)
-#     if i > 1 and i < 10 :
-#         mid.append(i)
+for i in my_list :
+    if i == 1 :
+        min.append(i)
+    if i == 10 :
+        max.append(i)
+    if i > 1 and i < 10 :
+        mid.append(i)
 
-# print(mid)  
+print(mid)  
         
 
-# list = [1,2,3,4,5,6,7,8,9,10]
+list = [1,2,3,4,5,6,7,8,9,10]
 
-# separator = []
+separator = []
 
-# for i in list :
-#     if i % 2 == 0 :
-#      separator.append(i)
-# print(separator)
-
-
-# list = [1,2,3,4,5,6,7,8,9,10]
-
-# separator = []
+for i in list :
+    if i % 2 == 0 :
+     separator.append(i)
+print(separator)
 
 
-# word = input("please enter any word")
+list = [1,2,3,4,5,6,7,8,9,10]
 
-# modified_word = ""
-
-
-# if word == word.lower() :
-#     print(word.upper())
-# elif word == word.upper() :
-#     print(word.lower())
+separator = []
 
 
+word = input("please enter any word")
+
+modified_word = ""
+
+
+if word == word.lower() :
+    print(word.upper())
+elif word == word.upper() :
+    print(word.lower())
 
 
 
 
 
 
+def most_frequent(numbers):
+    
+    frequency = {}
+    for num in numbers:
+        if num in frequency:
+            frequency[num] += 1
+        else:
+            frequency[num] = 1
 
+    
+    max_frequency = max(frequency.values())
 
+    return max_frequency
 
-
-
+numbers = [1, 2, 3, 2, 2, 1, 3, 3, 3, 2]
+print(most_frequent(numbers))
 
 
 
