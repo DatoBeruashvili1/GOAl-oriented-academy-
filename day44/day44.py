1bec2da57399d3ccb74e5ee89f27852fa082dd28
DatoBeruashvili1 <datobero011@gmail.com>
2024-07-21 16:28:42 +0400

    commit

---
+++ day44/day44.py
@@ -0,0 +1,80 @@
+#Python Syntax
+print("hello world")
+
+
+print(input("please enter your name:"))
+
+
+numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,19,20]
+
+for i in numbers : 
+    if i % 4 == 0 :
+        print(numbers)
+
+
+# python comments
+
+# Get the user's name
+name = input("What is your name? ")
+
+# Get the user's age
+age = int(input("How old are you? "))
+
+# Print out a greeting message
+print("Hello, " + name + "! You are " + str(age) + " years old.")
+
+# Check if the user is an adult
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are not an adult.")
+
+
+#lets calculate rectangles perimeter with a our user :D
+
+# Get the length and width from the user
+length = int(input("Enter the length of the rectangle: "))
+width = int(input("Enter the width of the rectangle: "))
+
+perimeter = (length * width)
+
+print(perimeter)
+
+#lets look at this code
+
+# print GOA
+
+#wrong right?
+
+#you may ask why its not working?
+
+#its not working because after print you need to open nails :D
+
+
+#Python varibles
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
