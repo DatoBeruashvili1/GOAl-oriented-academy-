dfa6b4a4e2837b7d6aa82b2ad382dff47b83cab2
DatoBeruashvili1 <datobero011@gmail.com>
2024-08-31 13:48:16 +0400

    commit

---
+++ day61/classwork/classwork.js
@@ -0,0 +1,12 @@
+const car = {
+    name: "porche",
+    createdAt: 1999,
+    printInfo() {
+      console.log(this.createdAt)
+    }
+  }
+  car.printInfo()
+  
+  
+  
+  
---
+++ day61/homework/script.js
@@ -0,0 +1,39 @@
+// const me = {
+//     name : "dato",
+//     age : "16",
+//     proffesion : "programming"
+// }
+
+// console.log(me)
+
+// const car = {
+//     name: "porche",
+//     model : "911-GT3",
+//     release_date : "1999"
+// }
+
+// console.log(car.name)
+
+// console.log(car.model)
+
+// console.log(car.release_date)
+
+
+// const person = {
+//     name: "zauri",
+//     age : "35"
+// }
+
+// person.age = 155
+
+// console.log(person)
+
+
+// me.hobby = "watching movies"
+
+// console.log(me)
+
+
+// delete me.proffesion
+
+
